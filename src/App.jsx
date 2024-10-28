fd7d936475254c072d4c5cf28945eaa134326538
Mesadova <emesadova@hotmail.com>
2024-10-28 16:28:01 +0100

    Level1-Ex4

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
 import { useState } from 'react'
-import Card from './components/Card'
+import AppCard from './components/Card'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   const [step, setStep] = useState(0)
   const tutorialData = [
     {
       title: 'Dedicate many hours',
-      description: "A minimum time of 30 weekly hours. If you don't have enough, you'll have to dedicate more hours. In the beginning it seems impossible, but you'll quickly notice improvements.",
-      bgColor: '',
-      image: '',
+      description: "A minimum time of 30 weekly hours. If you don't have enough, you'll have to dedicate more hours. In the beginning it seems impossible, but you'll notice rapid improvement.",
+      bgColor: '#4DAAAB',
+      image: 'src/assets/time_management.svg',
+    },
+    {
+      title: 'Work on your own projects',
+      description: "It's better to spend 10 hours working on your own projects than 10 hours watching tutorials. Motivation and involvement in the project will help you accelerate your learning.",
+      bgColor: '#D4D4D7',
+      image: 'src/assets/programming.svg',
+    },
+    {
+      title: 'Try to rest',
+      description: "Good rest and disconnecting are vital. This way, you'll reduce stress and anxiety. It will improve your concentration and solidify your learning.   \n ",
+      bgColor: '#F4D06F',
+      image: 'src/assets/meditation.svg',
     }
   ]
 
+  const nextStep = () => {
+    const addStep = step + 1
+    setStep(addStep)
+  }
+
+  const prevStep = () => {
+    const subStep = step -1
+    setStep(subStep)
+  }
+
+  const containerStyle = {
+    display: 'flex',
+    border: 'solid',
+    borderColor: 'red',
+    justifyContent: 'center',
+  }
+
   return (
-    <>
-      <Card tutorialData={tutorialData} step={step}/>
-    </> 
+    <div className='container' style={containerStyle}>
+      <AppCard tutorialData={tutorialData} step={step} nextStep={nextStep} prevStep={prevStep}/>
+    </div> 
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,195 @@
-const Card = (props) => {
+import { Button, Card, CardBody } from "react-bootstrap"
+import styled from 'styled-components'
+
+const AppCard = (props) => {
     const currentCardData = props.tutorialData[props.step]
+    const backgroundColor = currentCardData.bgColor
+
+    const left = `${props.step}` * 22
+
+    const StepContainer = styled.div`
+        display: flex;
+        position: relative;
+        
+        :after {
+            content: '';
+            position: absolute;
+            background: #4a154b;
+            height: 20%;
+            width: 42%;
+            top: 40%;
+            border-radius: 30%;
+            transition: 1.4s ease;
+            left: ${left}px;
+        }
+`
+
+    const StepWrapper = styled.div`
+        display: flex;
+        flex-direction: row;
+        border: solid;
+        border-color: white;
+        background-color: white;
+        gap: 12px;
+        align-items: center;
+        align-content: center;
+        position: relative;  
+    `
+
+    const StepStyle = styled.div`
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background-color: #A9A9A9;
+        transition: 1.4s ease;
+
+    `
+
+    const cardImgStyle = {
+        backgroundColor: backgroundColor,
+        paddingTop: '110px',
+        paddingBottom: '110px',
+        paddingLeft: '10px',
+        paddingRight: '10px',
+        minHeight: '420px',
+        maxHeight: '420px',
+    }
+    const cardStyle = {
+        marginTop: '30px',
+        marginBottom: '30px',
+        width: '25rem',
+        borderRadius: '50px',
+        overflow: 'hidden',
+        minHeight: '600px',
+        maxHeight: '800px'
+    }
+    const buttonStyle = {
+        borderRadius: '100px',
+        width: '55px',
+        height: '55px',
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+    }
+    const cardFooterStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: 'lightblue',
+    }
+    const arrowsStyle = {
+        fontSize: '55px'
+    }
+    const iconsStyle = {
+        fontSize: '55px',
+        display: 'inline-block',
+        gap: '0px',
+        margin: '0px',
+        padding: '0%',
+        border: 'solid',
+        borderColor: 'red',
+        alignItems: 'start',
+        justifyContent: 'start',
+        whiteSpace: 'no-wrap'
+    }
+    const buttonContainerStyle = {
+        display: 'flex',
+        wrap: 'no-wrap',
+        direction: 'rtl',
+        border: 'solid',
+        borderColor: 'white',
+        backgroundColor: 'white',
+        gap: '10px'
+    }
+    const cardTextStyle = {
+        fontFamily: 'Poppins',
+        fontSize: '14px',
+        paddingLeft: '10px',
+        paddingRight: '20px',
+        paddingBottom: '25px',
+        justifyContent: 'end',
+        textAlign: 'start',
+        color: 'DarkSlateGray',
+        whiteSpace: 'pre-wrap'
+    }
+    const cardTitleStyle = {
+        fontFamily: 'Poppins',
+        fontSize: '18px',
+        paddingTop: '15px',
+        paddingBottom: '10px',
+        paddingLeft: '10px'
+    }
+    
     return(
-      <div>
-        {Object.entries(currentCardData).map(([key, value]) => {
-          if (key === "title") {
-            return <h3 key={key}>{value}</h3>
-          } else if (key === "description") {
-            return <p key={key}>{value}</p>
-          }
-          return null
-        })}
-      </div>
+        <Card style={cardStyle}>
+            {Object.entries(currentCardData).map(([key, value]) => {
+                if (key === "image") {
+                    return <Card.Img key={key} variant="top" src={value} style={cardImgStyle}/>
+                } else { null }})}
+            <CardBody>
+            {Object.entries(currentCardData).map(([key, value]) => {
+                if (key === "title") {
+                    return <Card.Title key={key} style={cardTitleStyle}><b>{value}</b></Card.Title>
+                } else if (key === "description") {
+                    return <Card.Text key={key} style={cardTextStyle}>{value}</Card.Text>
+                } else { null }})}
+            {props.step === 0 ? (
+                <div style={cardFooterStyle}>
+                    <StepContainer>
+                        <StepWrapper>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                        </StepWrapper>
+                    </StepContainer>
+                    <div style={buttonContainerStyle}>
+                        <Button onClick={props.nextStep} style={buttonStyle} >
+                            <i className="bi bi-arrow-right-circle-fill text-dark" style={arrowsStyle}></i>
+                        </Button>
+                    </div>
+                </div>)
+            : props.step === 1 ? (
+                <div style={cardFooterStyle}>
+                    <StepContainer>
+                        <StepWrapper>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                        </StepWrapper>
+                    </StepContainer>
+                    <div style={buttonContainerStyle}>
+                        <Button onClick={props.nextStep} style={buttonStyle} >
+                            <i className="bi bi-arrow-right-circle-fill text-dark" style={arrowsStyle}></i>
+                        </Button>
+                        <Button onClick={props.prevStep} style={buttonStyle}>
+                            <i className="bi bi-arrow-left-circle text-dark" style={arrowsStyle}></i>
+                        </Button>
+                    </div>
+                </div>
+            ) : (
+                <div style={cardFooterStyle}>
+                    <StepContainer>
+                        <StepWrapper>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                            <StepStyle></StepStyle>
+                        </StepWrapper>
+                    </StepContainer>
+                    <div style={buttonContainerStyle}>
+                        <Button onClick={props.prevStep} style={buttonStyle}>
+                            <i className="bi bi-arrow-left-circle text-dark" style={arrowsStyle}></i>
+                        </Button>
+                    </div>
+                </div>
+             ) }
+            
+            </CardBody>
+        </Card>
     )
 }
 
 
-export default Card
+export default AppCard
